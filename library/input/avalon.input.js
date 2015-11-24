b7ebbe911752e3787ced4c8992546fe1112ad322
ttt22703 <ttt22703@hotmail.com>
2015-11-24 15:25:30 +0800

    测试文件

--- library/input/avalon.input.js
+++ library/input/avalon.input.js
@@ -2,16 +2,56 @@ define(["avalon", "text!./avalon.input.html", "css!./avalon.input.css"], functio
 
     var _interface = function () {
     };
+
+    function checkTextType(type, value)
+    {
+        switch (type)
+        {
+            case "decimal":
+                if (!/^\-?\d*\.?\d+$/.test(value))
+                {
+                    return "请输入小数";
+                }
+                break;
+            case "int":
+                if (!/^\-?\d+$/.test(value))
+                {
+                    return  "请输入整型数字";
+                }
+                break;
+            case "chs":
+                if (!/^[\u4e00-\u9fa5]+$/.test(value))
+                {
+                    return "请输入中文";
+                }
+                break;
+            case "url":
+                if (!/^(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/.test(value))
+                {
+                    return "请输入正确的网址";
+                }
+                break;
+            case "email":
+                if (!/^([A-Z0-9]+[_|\_|\.]?)*[A-Z0-9]+@([A-Z0-9]+[_|\_|\.]?)*[A-Z0-9]+\.[A-Z]{2,3}$/i.test(value))
+                {
+                    return "请输入正确的email";
+                }
+                break;
+            default:
+                return  "不支持的验证类型";
+                break;
+        }
+    }
+
     avalon.component("ms:input", {
         //内部变量
-//        _readonly: false,
-//        _disabled: false,
         _notice: "",
-        _regexErrorNotice:"",
+        _regexErrorNotice: "",
         //内部方法
         _blur: _interface,
         _focus: _interface,
-        onInit:function(){},//必须定义此接口
+        onInit: _interface, //必须定义此接口
+        _check:_interface,
         //配置项
         title: "",
         value: "",
@@ -21,27 +61,25 @@ define(["avalon", "text!./avalon.input.html", "css!./avalon.input.css"], functio
         isReadonly: false,
         widthType: "normal",
         //自定义正则表达式
-        regexContet: "", 
+        regexContet: "",
         //文本类型
-        textType:"",
+        textType: "",
         //类型校验错误信息
-        regexErrorNotice:"请输入正确的类型",
+        regexErrorNotice: "",
         //模板
         $template: template,
         //替换自定义标签
         $replace: 1,
-
         $construct: function (defaultConfig, vmConfig, eleConfig) {
             var options = avalon.mix(defaultConfig, vmConfig, eleConfig)
             return options
         },
-
         $init: function (vm) {
             vm._notice = vm.notice;
-            vm._regexErrorNotice=vm.regexErrorNotice;
-            vm.regexErrorNotice="";
+            vm._regexErrorNotice = vm.regexErrorNotice;
+            vm.regexErrorNotice = "";
             vm.title = vm.title + "：";
-            if (vm.isRequired && vm.value=="")
+            if (vm.isRequired && vm.value.trim() == "")
             {
                 vm.notice = "*";
             }
@@ -52,48 +90,51 @@ define(["avalon", "text!./avalon.input.html", "css!./avalon.input.css"], functio
         },
         $ready: function (vm, element) {
             vm.onInit(vm);
-            vm._blur = function ()
+            vm._check=function()
             {
-                if (vm.isRequired && this.value == "")
+                 if (vm.isRequired && this.value.trim() == "")
                 {
                     vm.notice = vm._notice;
+                    return;
                 }
                 else
                 {
                     vm.notice = "";
                 }
-                
+
                 //校验类型
-                if(vm.textType!="")
+                vm.regexErrorNotice = "";
+                if (vm.textType != "" && this.value.trim() != "")
                 {
-                    switch(vm.textType)
+                    vm.regexErrorNotice = checkTextType(vm.textType, this.value);
+                    if (vm.regexErrorNotice != "")
                     {
-                        case "number":
-                            break;
-                        case "int":
-                            break;
-                        default:
-                            vm.regexErrorNotice="不支持的验证类型"
-                            break;
-                    }
-                    if(vm.regexErrorNotice!="")
-                    {
-                        vm.notice=vm.regexErrorNotice;
+                        if (vm._regexErrorNotice != "")
+                        {
+                            vm.notice = vm._regexErrorNotice;
+                        }
+                        else
+                        {
+                            vm.notice = vm.regexErrorNotice;
+                        }
                     }
                 }
-                
+            }
+            vm._blur = function ()
+            {
+               vm._check();
             }
             vm._focus = function ()
             {
 
             }
-//            vm.$watch("isDisabled",function(n,o){
-//                alert(n)
-//            })
+            //            vm.$watch("isDisabled",function(n,o){
+            //                alert(n)
+            //            })
         },
-        $dispose:function(vm,element)
+        $dispose: function (vm, element)
         {
-            element.innerHTML="";
+            element.innerHTML = "";
         }
 
     })
